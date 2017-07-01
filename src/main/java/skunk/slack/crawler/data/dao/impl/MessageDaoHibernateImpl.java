a894b34df527c85e6b9d42f693c0c2bf3c870dd5
miyake <miyake.y@me.com>
2017-07-02 01:20:24 +0900

    improved search function

--- src/main/java/skunk/slack/crawler/data/dao/impl/MessageDaoHibernateImpl.java
+++ src/main/java/skunk/slack/crawler/data/dao/impl/MessageDaoHibernateImpl.java
@@ -2,7 +2,9 @@ package skunk.slack.crawler.data.dao.impl;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -18,16 +20,18 @@ import skunk.slack.crawler.data.core.hibernate.SessionFactory;
 import skunk.slack.crawler.data.dao.spec.MessageDao;
 import skunk.slack.crawler.data.entity.model.Channel;
 import skunk.slack.crawler.data.entity.model.Message;
+import skunk.slack.crawler.data.entity.model.User;
 
 @Slf4j
 public class MessageDaoHibernateImpl extends AbstractEntityDaoHibernateImpl<Message> implements MessageDao {
 	private static final Pattern KEYWORD = Pattern.compile("[^\")';%]+");
+	private static final UserDaoHibernateImpl userDao = new UserDaoHibernateImpl();
 
 	@SuppressWarnings({ "unchecked", "deprecation" })
 	@Override
 	public List<Message> getInRange(Channel channel, Integer count, String maxTs) {
 		try (Session session = SessionFactory.openSession();) {
-			// omit duplicated Message with distinct 
+			// omit duplicated Message with distinct
 			// because multiple Message is created if multiple referenced_users exist
 			return (List<Message>) session.createCriteria(Message.class).add(Restrictions.eq("channel", channel))
 					.add(Restrictions.le("ts", maxTs)).addOrder(Order.desc("ts")).setMaxResults(count).list().stream()
@@ -47,29 +51,63 @@ public class MessageDaoHibernateImpl extends AbstractEntityDaoHibernateImpl<Mess
 	@SuppressWarnings({ "deprecation", "unchecked" })
 	@Override
 	public List<Message> search(Collection<String> keywords, Collection<String> channelIds) {
-		try (Session session = SessionFactory.openSession();) {
-			StringBuilder sql = new StringBuilder();
-			sql.append("SELECT * FROM message m ").append("LEFT JOIN user u ON m.user_id = u.id WHERE ");
-			boolean isFirstKeyword = true;
-			for (String keyword : keywords) {
-				if (!KEYWORD.matcher(keyword).matches()) {
-					continue;
-				}
-				if (isFirstKeyword) {
-					isFirstKeyword = false;
-				} else {
-					sql.append("AND ");
-				}
-				sql.append("(m.text LIKE '%").append(keyword).append("%' OR u.name = '").append(keyword).append("') ");
-			}
-			sql.append("AND m.channel_id IN ('").append(Joiner.on("','").join(channelIds)).append("') ");
-			sql.append("ORDER BY m.ts DESC");
+		List<User> users = userDao.getUsers(keywords);
+		Set<String> userIds = new HashSet<>();
+		Set<String> userNames = new HashSet<>();
+		Set<String> channelIdSet = new HashSet<>(channelIds);
+		for (User user : users) {
+			userIds.add(user.getId());
+			userNames.add(user.getName());
+		}
+		Set<String> keywordSet = keywords.stream().filter(k -> !userNames.contains(k)).collect(Collectors.toSet());
 
-			return session.createSQLQuery(sql.toString()).addEntity(Message.class).list();
+		try (Session session = SessionFactory.openSession();) {
+			String sql = createSql(keywordSet, userIds, channelIdSet);
+			return (List<Message>) session.createSQLQuery(sql.toString()).addEntity(Message.class).list().stream()
+					.distinct().collect(Collectors.toList());
 		} catch (HibernateException | IOException e) {
 			log.error("Failed to retrieve messages [channels:{},keywords:{}]", channelIds, keywords);
 			log.error("", e);
 			return null;
 		}
+	}
+
+	private String createSql(Set<String> keywords, Set<String> userIds, Set<String> channelIds) {
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT * FROM message m ");
+		if (!userIds.isEmpty()) {
+			sql.append("LEFT JOIN message__user u ON m.message_type = u.message_message_type AND m.ts = u.message_ts ");
+		}
+		sql.append("WHERE (");
+		if (!keywords.isEmpty()) {
+			sql.append("(");
+		}
+		boolean isFirstKeyword = true;
+		for (String keyword : keywords) {
+			if (!KEYWORD.matcher(keyword).matches()) {
+				continue;
+			}
+			if (isFirstKeyword) {
+				isFirstKeyword = false;
+			} else {
+				sql.append("AND ");
+			}
+			sql.append("m.text LIKE '%").append(keyword).append("%' ");
+		}
+		if (!keywords.isEmpty()) {
+			sql.append(") ");
+		}
+		if (!keywords.isEmpty() && !userIds.isEmpty()) {
+			sql.append("AND ");
+		}
+		if (!userIds.isEmpty()) {
+			String userIdsStr = new StringBuilder("('").append(Joiner.on("','").join(userIds)).append("') ").toString();
+			sql.append("(m.user_id IN ").append(userIdsStr);
+			sql.append("OR u.referenced_users_id IN ").append(userIdsStr).append(")");
+		}
+		sql.append(") AND m.channel_id IN ('").append(Joiner.on("','").join(channelIds)).append("') ");
+		sql.append("ORDER BY m.ts DESC");
+
+		return sql.toString();
 	}
 }
--- src/main/java/skunk/slack/crawler/data/dao/impl/UserDaoHibernateImpl.java
+++ src/main/java/skunk/slack/crawler/data/dao/impl/UserDaoHibernateImpl.java
@@ -1,11 +1,32 @@
 package skunk.slack.crawler.data.dao.impl;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import lombok.extern.slf4j.Slf4j;
+import skunk.slack.crawler.data.core.hibernate.SessionFactory;
 import skunk.slack.crawler.data.dao.spec.UserDao;
 import skunk.slack.crawler.data.entity.model.User;
 
+@Slf4j
 public class UserDaoHibernateImpl extends AbstractEntityDaoHibernateImpl<User> implements UserDao {
 	@Override
 	Class<User> getEntityClass() {
 		return User.class;
+	}
+	
+	@SuppressWarnings({ "deprecation", "unchecked" })
+	List<User> getUsers(Collection<String> userNames) {
+		try (Session session = SessionFactory.openSession();) {
+			return session.createCriteria(User.class).add(Restrictions.in("name", userNames)).list();
+		} catch (HibernateException | IOException e) {
+			log.error("Failed to retrieve users [userNames:{}]", userNames, e);
+			return null;
+		}
 	}
 }
