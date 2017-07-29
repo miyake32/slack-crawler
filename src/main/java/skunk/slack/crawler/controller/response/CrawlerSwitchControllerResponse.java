0b351bd3419bf99686913d012b32833e26f4fe5c
miyake_yut <miyake.y@me.com>
2017-07-29 20:54:41 +0900

    fixed warnings

--- src/main/java/skunk/slack/crawler/controller/response/CrawlerSwitchControllerResponse.java
+++ src/main/java/skunk/slack/crawler/controller/response/CrawlerSwitchControllerResponse.java
@@ -3,6 +3,7 @@ package skunk.slack.crawler.controller.response;
 import lombok.Data;
 import lombok.experimental.Builder;
 
+@SuppressWarnings("deprecation")
 @Builder
 @Data
 public class CrawlerSwitchControllerResponse {
--- src/main/java/skunk/slack/crawler/service/data/ChannelService.java
+++ src/main/java/skunk/slack/crawler/service/data/ChannelService.java
@@ -1,6 +1,5 @@
 package skunk.slack.crawler.service.data;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
